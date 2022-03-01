e8a686d8283df876e24b3515862504138a898e4e
Jasper Ginn <jasperginn@gmail.com>
2022-03-01 08:28:34 +0100

    replace echo statements with logger statements

--- src/client.nim
+++ src/client.nim
@@ -1,14 +1,17 @@
 # Client application for chat server
 
 import os, threadpool
-echo("Chat application started")
+import std/logging
+
+var logger = newConsoleLogger()
+logger.log(lvlInfo, "Chat application started")
 
 if paramCount() == 0:
   quit("Please specify the server address, e.g. './client localhost'")
 
 let serverAddr = paramStr(1)
-echo("Connecting to ", serverAddr)
+logger.log(lvlInfo, "Connecting to " & serverAddr)
 
 while true:
   let message = spawn stdin.readLine()
-  echo("Sending \"", ^message, "\"")
+  logger.log(lvlInfo, "Sending \"" & ^message & "\"")
